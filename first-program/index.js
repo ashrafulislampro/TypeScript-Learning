e8f8634b9c3a8ed6f45cfc923ffad9fc025953b7
ashrafulislampro <ashrafulislam1659@gmail.com>
2023-11-19 16:46:40 +0600

    first typescript program

--- first-program/index.js
+++ first-program/index.js
@@ -1,3 +1,8 @@
 // tsc index.ts -> index.js -> node index.js
+function addNumbers(num1, num2) {
+    console.log(num1 + num2);
+}
+addNumbers(20, 30);
+addNumbers(20, 50);
 var userName = "Ashraful Islam";
 console.log(userName);
--- first-program/index.ts
+++ first-program/index.ts
@@ -1,3 +1,9 @@
 // tsc index.ts -> index.js -> node index.js
+
+function addNumbers(num1: number, num2: number) {
+  console.log(num1 + num2);
+}
+addNumbers(20, 30);
+addNumbers(20, 50);
 let userName = "Ashraful Islam";
 console.log(userName);
