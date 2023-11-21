877332af0135e8dbbf399a6f6a99bdda8d31adc4
ashrafulislampro <ashrafulislam1659@gmail.com>
2023-11-21 23:12:41 +0600

    tuple-type is complete

---
+++ user-defined-type/tuple-type.js
@@ -0,0 +1,5 @@
+// mixed data - key, value
+var user;
+user = [101, "anis"];
+user.push(102, "ashraful");
+console.log(user);
---
+++ user-defined-type/tuple-type.ts
@@ -0,0 +1,8 @@
+// mixed data - key, value
+
+let user: [number, string];
+user = [101, "anis"];
+
+user.push(102, "ashraful");
+
+console.log(user);
