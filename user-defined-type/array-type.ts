f6d34117a8a4716ce460d80005a6bd6753d0cc5a
ashrafulislampro <ashrafulislam1659@gmail.com>
2023-11-21 22:54:45 +0600

    array-type practice and revision done

--- user-defined-type/array-type.ts
+++ user-defined-type/array-type.ts
@@ -1,5 +1,5 @@
-// let userNames: string[];
-let userNames: Array<string>;
-userNames = ["ashraful", "tahmina", "homayra"];
+let userNames: string[];
+// let userNames: Array<string>;
+userNames = ["ashraful", "tahmina", "homayra", "Umama"];
 
 console.log(userNames[0]);
